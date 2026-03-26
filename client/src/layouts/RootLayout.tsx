3e02ce5516d52f2cd076faaa876fca32969a6884
Thomas Ho <khoaho120703@gmail.com>
2026-03-26 20:21:54 +0700

    [client] feat: Footer

--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 export default function RootLayout() {
   return (
     <div className="min-h-[70vh]">
       <Outlet />
+      <Footer />
     </div>
   );
 }
