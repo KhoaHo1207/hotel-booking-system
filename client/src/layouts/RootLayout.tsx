b424232bb4b96637190ea077fa9e97724ac88ddf
Thomas Ho <khoaho120703@gmail.com>
2026-03-27 16:02:43 +0700

    [client] feat: Hotel Registration

--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
+import HotelReg from "../components/HotelReg";
 
 export default function RootLayout() {
   return (
     <>
+      <HotelReg />
       <div className="min-h-[calc(100vh-10rem)]">
         <Outlet />
       </div>
