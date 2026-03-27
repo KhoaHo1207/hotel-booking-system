590d049351d83a34178aa79119353090c7b728b5
Thomas Ho <khoaho120703@gmail.com>
2026-03-27 17:27:24 +0700

    [client] feat: List Rooms Page

--- client/src/pages/hotelOwner/ListRoom.tsx
+++ client/src/pages/hotelOwner/ListRoom.tsx
@@ -1,3 +1,68 @@
+import { useState } from "react";
+import type { Room } from "../../types";
+import { roomsDummyData } from "../../assets/assets";
+import Title from "../../components/Title";
+
 export default function ListRoom() {
-  return <div>ListRoom</div>;
+  const [rooms, setRooms] = useState<Room[]>(roomsDummyData);
+  return (
+    <div>
+      <Title
+        align="left"
+        title="Room Listings"
+        subTitle="View, edit, or manage all listed rooms. Keep the information up-to-date to provide the best experience for users."
+        font="outfit"
+      />
+
+      <p className="text-gray-500 mt-8">All Rooms</p>
+
+      <div className="w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-96 overflow-y-scroll mt-3">
+        <table className="w-full">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="py-3 px-4 text-gray-800 font-medium">Name</th>
+              <th className="py-3 px-4 text-gray-800 font-medium max-sm:hidden">
+                Facility
+              </th>
+              <th className="py-3 px-4 text-gray-800 font-medium">
+                Price/ night
+              </th>
+              <th className="py-3 px-4 text-gray-800 font-medium text-center">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {rooms.map((room, index) => (
+              <tr key={index}>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                  {room.roomType}
+                </td>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300 max-sm:hidden">
+                  {room.amenities.join(", ")}
+                </td>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                  {room.pricePerNight}
+                </td>
+                <td className="py-3 px-4 border-t border-gray-300 text-center">
+                  <label
+                    htmlFor=""
+                    className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3"
+                  >
+                    <input
+                      type="checkbox"
+                      className="sr-only peer"
+                      checked={room.isAvailable}
+                    />
+                    <div className="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200"></div>
+                    <span className="dot absolute left-1 top-1 size-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
+                  </label>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
